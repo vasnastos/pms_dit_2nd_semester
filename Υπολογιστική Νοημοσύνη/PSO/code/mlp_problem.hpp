db45e2380308eb9528676e71ee611c9edbaec9b6
NASTOS VASILEIOS <63415169+vasnastos@users.noreply.github.com>
2023-05-12 18:42:52 +0300

    update MLP

--- Υπολογιστική Νοημοσύνη/PSO/code/mlp_problem.hpp
+++ Υπολογιστική Νοημοσύνη/PSO/code/mlp_problem.hpp
@@ -1,8 +1,13 @@
 #include "problem.h"
+#include "dataset.h"
 
 class MlpProblem
 {
     private:
+        Dataset *d;
+        int nodes;
         vector <double> weights;
-        
+    public:
+        MlpProblem(Dataset *d);
+        ~MlpProblem();
 };
